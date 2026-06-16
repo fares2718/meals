a8c45d2fa186d71c5a249bbdd3e8ec7fb87e7dd5
fares2718 <faresobaid2715@gmail.com>
2026-06-16 17:27:49 +0300

    feat: implement tabs for categories and favorites, add meal details screen

--- lib/Screens/categories.dart
+++ lib/Screens/categories.dart
@@ -5,21 +5,21 @@ import 'package:meals/models/meal.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, String title, List<Meal> meals) {
     Navigator.of(context).push(
       MaterialPageRoute<void>(
-        builder: (context) => MealsScreen(title: title, meals: meals),
+        builder: (context) => MealsScreen(title: title, meals: meals,onToggleFavorite: onToggleFavorite,),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Pick your category')),
-      body: GridView(
+    return GridView(
         padding: const EdgeInsets.all(20),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -43,7 +43,6 @@ class CategoriesScreen extends StatelessWidget {
               ),
             )
             .toList(),
-      ),
-    );
+      );
   }
 }
